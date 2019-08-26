559f0c9c47f08ab9bc36538a9efec668abeb4a55
Happy <yymqqc@126.com>
2019-08-26 08:33:43 +0800

    t24 var01 复习2019年8月26日

---
+++ src/main/java/com/openmind/day01/T1.java
@@ -0,0 +1,13 @@
+package com.openmind.day01;
+
+/**
+ * @Classname T1
+ * @Description 微冷的雨训练营 www.cnblogs.com/weilengdeyu
+ * @Date 2019/8/26 8:33
+ * @Created by Happy-微冷的雨
+ */
+public class T1 {
+    public static void main(String[] args) {
+        //Var01
+    }
+}
